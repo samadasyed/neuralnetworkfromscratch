45f14376f243728255b2503be5a032ae2f149385
samadasyed <samadasyed@hotmail.com>
2026-02-24 20:27:09 -0600

    Fixed hyphen issue in the starter code

--- readinginMNIST.py
+++ readinginMNIST.py
@@ -14,10 +14,10 @@ def load_labels(filename):
         labels = np.frombuffer(f.read(), dtype=np.uint8)
         return labels
         
-X_train = load_images("train-images.idx3-ubyte")
-y_train = load_labels("train-labels.idx1-ubyte")
-X_test = load_images("t10k-images.idx3-ubyte")
-y_test = load_labels("t10k-labels.idx1-ubyte")
+X_train = load_images("train-images-idx3-ubyte")
+y_train = load_labels("train-labels-idx1-ubyte")
+X_test = load_images("t10k-images-idx3-ubyte")
+y_test = load_labels("t10k-labels-idx1-ubyte")
 # Flatten
 X_train = X_train.reshape(X_train.shape[0], -1)
 X_test = X_test.reshape(X_test.shape[0], -1)
